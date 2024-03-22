2764b601b6022cc12e5e9670980885cb823dee88
Rand-Sai <yuan.sa@northeastern.edu>
2024-03-22 12:09:15 -0400

    Create Class Employee with initliizer

--- Assignment06_Problem02_Yuan.py
+++ Assignment06_Problem02_Yuan.py
@@ -5,3 +5,12 @@
 #SalariedEmployee, and CommissionEmployee that each have different implementations of the
 #calculate_salary() method. Implement a function that processes a list of employees and calculates their
 #salary using polymorphism, without needing to check the type of each employee.
+
+class Employee:
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+    def calculate_salary(self):
+        pass
+
